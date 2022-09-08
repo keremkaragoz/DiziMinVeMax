fb0069c0c181a524890662815f7f1122d7cc044c
keremkaragoz <108470789+keremkaragoz@users.noreply.github.com>
2022-09-08 09:26:33 +0300

    Add files via upload

---
+++ MinVeMax.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class MinVeMax {
+    public static void main(String[] args) {
+        int number;
+        int[] array = {15,12,788,1,-1,-778,2,0};
+        System.out.println("Dizi:" +Arrays.toString(array));
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Sayı giriniz:");
+        number = input.nextInt();
+
+        Arrays.sort(array);
+
+        for(int i = 0; i < array.length; i++) {
+            if(array[i] > number) {
+                System.out.println("Girilen sayıdan küçük en yakın sayı: " + array[i -2]);
+                break;
+            }
+        }
+
+        for(int j = 0; j <  array.length; j++) {
+            if(array[j] > number) {
+                System.out.println("Girilen sayıdan büyük en yakın sayı: " + array[j]);
+                break;
+            }
+        }
+    }
+}
